cf013cf9b51e6a18317d5256f1d2cba234b1c133
Parv Kumar <parvathikumar92@gmail.com>
2017-10-23 23:41:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+class Driver{
+  constructor(name, start){
+    this.name = name
+    let msec = Date.parse(start)
+    this.startDate = new Date(msec)
 
+  }
+  yearsExperienceFromBeginningOf(end){
+    return (end - (this.startDate.getFullYear()+1))
+  }
+
+}
+
+class Route{
+  constructor(beg,end){
+    this.blocksTravelled = function(){
+      if (beg.horizontal === end.horizontal){
+        let total = Math.abs(beg.vertical - end.vertical)
+        return total
+      }
+      else if (beg.vertical === end.vertical){
+        return 4
+      }
+      else {
+        let total = Math.abs(beg.vertical - end.vertical) + 4
+        return total
+      }
+
+    }
+    this.estimatedTime = function(peakHours){
+      if (peakHours){
+        return (this.blocksTravelled()/2)
+      }
+      else{
+        return (this.blocksTravelled()/3)
+      }
+    }
+  }
+
+}
